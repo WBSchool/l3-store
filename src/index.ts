faf1220f5eca7f943a6ad0b8f34daa328b5646aa
DashinIN <green19grant@yandex.ru>
2023-10-19 09:43:49 +0300

    исправил стили ссылок и подключил сервис избранного

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import "./icons";
 import Router from "./router";
 import { cartService } from "./services/cart.service";
+import { selectService } from "./services/select.service";
 import { userService } from "./services/user.service";
 
 new Router();
 cartService.init();
+selectService.init();
 userService.init();
 
 setTimeout(() => {

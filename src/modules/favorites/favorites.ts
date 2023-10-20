62e12e08f5e60f9721cbf0f3843891ebaf1803a0
Andrey Dagaev <dandr212@gmail.com>
2023-10-20 16:05:39 +0300

    [~] add new favorite-page view

--- src/modules/favorites/favorites.ts
+++ src/modules/favorites/favorites.ts
@@ -1,36 +1,26 @@
 import { Component } from '../component';
-import { Product } from '../product/product';
 import html from './favorites.tpl.html';
-import { formatPrice } from '../../utils/helpers';
 import { ProductData } from 'types';
 import { favoritesService } from '../../services/favorites.service';
+import { ProductList } from '../productList/productList';
 
 class Favorites extends Component {
-  products!: ProductData[];
+  favoriteProducts!: ProductData[];
+  favoritesProductsList: ProductList;
 
+  constructor(props: any) {
+    super(props);
+    this.favoritesProductsList = new ProductList();
+    this.favoritesProductsList.attach(this.view.popular);
+  }
   async render() {
-    this.products = await favoritesService.get();
+    this.favoriteProducts = await favoritesService.get();
     
-    if (this.products.length < 1) {
+    if (this.favoriteProducts.length < 1) {
       this.view.root.classList.add('is__empty');
       return;
     }
-
-    this.products.forEach((product) => {
-      const productComp = new Product(product, { isHorizontal: true });
-      productComp.render();
-      productComp.attach(this.view.cart);
-    });
-
-    const totalPrice = this.products.reduce((acc, product) => (acc += product.salePriceU), 0);
-    this.view.price.innerText = formatPrice(totalPrice);
-
-    this.view.btnClear.onclick = this._clearFavorites.bind(this);
-  }
-
-  private async _clearFavorites() {
-    await favoritesService.clear();
-    await this.render()
+    this.favoritesProductsList.update(this.favoriteProducts);
   }
 }
 

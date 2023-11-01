c8eb4c6050ec7e13b9b51ca4374739fdbc61c63e
Danil Grachev <jr9160260034@gmail.com>
2023-11-01 19:44:39 +0300

    added ability to test hints module

--- src/modules/searchHints/searchHints.ts
+++ src/modules/searchHints/searchHints.ts
@@ -1,10 +1,11 @@
 import { ViewTemplate } from '../../utils/viewTemplate';
 import { View } from '../../utils/view';
 import html from './searchHints.tpl.html';
+import {ProductData} from "../../../types";
 
 export class SearchHints {
   view: View;
-  suggestions: any[] = [];
+  suggestions: {name: string, link: string}[] = [];
 
   constructor(html: any) {
     this.view = new ViewTemplate(html).cloneView();
@@ -12,13 +13,42 @@ export class SearchHints {
   }
 
   async getSuggestions() {
-    const suggestions = [
+    // Для тестирования модуля. Получаем 3 рандомных объекта.
+    /*let suggestions: {name: string, link: string}[] = await this.getRandomSuggestions();
+    if (suggestions.length > 0) {
+      this.update(suggestions);
+      return;
+    }*/
+
+    const MockSuggestions = [
       {name: 'чехол iphone 13 pro', link: '#'},
       {name: 'коляски agex', link: '#'},
       {name: 'яндекс станция 2', link: '#'}
     ];
+    this.update(MockSuggestions);
+  }
 
-    this.update(suggestions);
+  async getRandomSuggestions() {
+    let products: ProductData[] = await fetch('/api/getPopularProducts').then(response => response.json());
+
+    if (products && products.length > 0) {
+      // Пропускаем те, названия которых составляют не больше слов чем указано
+      products = products.filter((item: ProductData) => (item.name.split(' ').length <= 3));
+    } else {
+      return [];
+    }
+
+    const suggestions = [];
+    for (let i = 0; i < 3; i++) {
+      const {name, id} = products[Math.floor(Math.random() * products.length)];
+      suggestions.push({
+        name,
+        link: `/product?id=${id}`
+      });
+    }
+
+    console.log('suggestions[]', suggestions);
+    return suggestions;
   }
 
   attach($root: HTMLElement) {

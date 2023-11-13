a26a64fb1a0a622d17d20f5cbaed739e5b44b248
IgorGuryanov <91266684+IgorGuryanov@users.noreply.github.com>
2023-11-13 12:25:27 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "./icons";
 import Router from "./router";
 import { cartService } from "./services/cart.service";
+import { favoriteProductsService } from "./services/favoriteProducts.service";
 import { userService } from "./services/user.service";
 
 new Router();
 cartService.init();
 userService.init();
+favoriteProductsService.init();
 
 setTimeout(() => {
   document.body.classList.add("is__ready");

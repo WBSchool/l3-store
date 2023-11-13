27cc194fb786932ed0a842ee766664a29f15499e
IgorGuryanov <91266684+IgorGuryanov@users.noreply.github.com>
2023-11-13 12:44:57 +0500

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import { homepageComp } from './modules/homepage/homepage';
 import { productDetailComp } from './modules/productDetail/productDetail';
 import { checkoutComp } from './modules/checkout/checkout';
 import { favoriteProductsComp } from './modules/favoriteProducts/favoriteProducts';
+import { eventAnalytics, typeEvent } from './services/event_analytics.service';
 
 const ROUTES = {
   '/': homepageComp,
@@ -24,11 +25,18 @@ export default class Router {
     window.addEventListener('hashchange', this.route.bind(this));
   }
 
-  route(e: any) {
+  async route(e: any) {
     e.preventDefault();
 
     // @ts-ignore
     const component = ROUTES[window.location.pathname] || notFoundComp;
+
+    const url: string = window.location.pathname;
+    await eventAnalytics.sendEvent({
+      type: typeEvent.route,
+      payload: { url },
+      timestamp: Date.now(),
+    });
 
     component.attach(this.$appRoot);
     component.render();

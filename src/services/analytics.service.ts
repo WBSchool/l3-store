ecb163546cd58bb0512399e6cd53e71b0e6ecf5b
FOND645 <fond645@gmail.com>
2023-12-19 15:30:57 +0300

    Сделаны остальные методы отправки аналитики

--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -1,3 +1,5 @@
+import { ProductData } from 'types';
+
 type AnalyticObj = {
   type: 'route' | 'viewCard' | 'addToCard' | 'purchase';
   payload: {
@@ -12,6 +14,42 @@ export const sendAnalytic = {
       type: 'route',
       payload: {
         url
+      },
+      timestamp: Date.now()
+    };
+    fetch('/api/sendEvent', { method: 'POST', body: JSON.stringify(obj) });
+  },
+
+  ViewCard(Product: ProductData, secretKey: string) {
+    const obj: AnalyticObj = {
+      type: 'viewCard',
+      payload: {
+        ...Product,
+        secretKey
+      },
+      timestamp: Date.now()
+    };
+    fetch('/api/sendEvent', { method: 'POST', body: JSON.stringify(obj) });
+  },
+
+  AddToCart(Product: ProductData) {
+    const obj: AnalyticObj = {
+      type: 'addToCard',
+      payload: {
+        ...Product
+      },
+      timestamp: Date.now()
+    };
+    fetch('/api/sendEvent', { method: 'POST', body: JSON.stringify(obj) });
+  },
+
+  Purchase(orderID: number, totalPrice: number, productIds: number[]) {
+    const obj: AnalyticObj = {
+      type: 'addToCard',
+      payload: {
+        orderID,
+        totalPrice,
+        productIds
       },
       timestamp: Date.now()
     };

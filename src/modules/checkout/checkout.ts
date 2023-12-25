6c064b0954c1fbec7906d9ccd3f7f4406a423af2
Vladislav <melnikovv717@gmail.com>
2023-12-25 22:00:47 +0300

    Добавлена функция отправки аналитики

--- src/modules/checkout/checkout.ts
+++ src/modules/checkout/checkout.ts
@@ -4,9 +4,19 @@ import html from './checkout.tpl.html';
 import { formatPrice } from '../../utils/helpers';
 import { cartService } from '../../services/cart.service';
 import { ProductData } from 'types';
+import { analyticsService } from '../../utils/analytics';
+import { genUUID } from '../../utils/helpers';
 
 class Checkout extends Component {
   products!: ProductData[];
+  totalPrice: number;
+  productIds: number[];
+
+  constructor(props: any) {
+    super(props)
+    this.totalPrice = 0
+    this.productIds = [];
+  }
 
   async render() {
     this.products = await cartService.get();
@@ -28,13 +38,29 @@ class Checkout extends Component {
     this.view.btnOrder.onclick = this._makeOrder.bind(this);
   }
 
+  _getProductIds() {
+    const productIds: number[] = []
+    this.products.forEach((product) => {
+      productIds.push(product.id)
+    });
+
+    return productIds
+  }
+
   private async _makeOrder() {
     await cartService.clear();
     fetch('/api/makeOrder', {
       method: 'POST',
       body: JSON.stringify(this.products)
     });
-    window.location.href = '/?isSuccessOrder';
+
+    analyticsService('purchase', {
+      orderId: genUUID(),
+      totalPrice: this.totalPrice,
+      productIds: this._getProductIds(),
+    })
+
+   // window.location.href = '/?isSuccessOrder';
   }
 }
 

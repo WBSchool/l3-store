9b660f0b47e0decd068ca91bf4bf2902f4df99ed
Алёна Ващан <alena_iv1998@mail.ru>
2024-02-05 10:51:20 +0300

    updated search page

--- src/modules/search/search.ts
+++ src/modules/search/search.ts
@@ -1,20 +1,56 @@
-import html from './search.tpl.html';
 import { Component } from '../component';
-import { View } from '../../utils/view';
-import { ViewTemplate } from '../../utils/viewTemplate';
+import html from './search.tpl.html';
 
-class SearchPage extends Component {
-  view: View;
+type Product = {
+  url: string;
+  name: string;
+  id?: string;
+};
 
-  constructor(props: any) {
-    super(props);
-    this.view = new ViewTemplate(html).cloneView();
+export class SearchTips extends Component {
+  tips: Product[] = [];
+
+  async render() {
+    const searchInput = document.getElementById('goodsSearch') as HTMLInputElement;
+    const searchUl = document.querySelector('.options') as HTMLUListElement;
+    const hideSearchBox = document.querySelector('.search__tips-box') as HTMLDivElement;
+    const goodsTips = document.querySelector('.search_box-tips') as HTMLDivElement;
+
+    searchInput.addEventListener('input', () => {
+      const options = this.getOptions(searchInput.value.trim(), this.tips);
+      const html = options
+        .map((products: Product) => {
+          return `<li class='search__link' ><a href=${products.url}>${products.name}</a></li>`;
+        })
+        .slice(0, 5)
+        .join('');
+
+      searchUl.innerHTML = html;
+      if (searchInput.value.trim() !== '') {
+        goodsTips.classList.add('hide');
+        hideSearchBox.classList.remove('hide');
+      } else {
+        goodsTips.classList.remove('hide');
+        hideSearchBox.classList.add('hide');
+      }
+    });
+
+    await fetch('/api/getProducts')
+      .then((res) => res.json())
+      .then((data) => {
+        this.tips = data.map((item: Product) => ({
+          name: item.name,
+          url: `product?id=${item.id}`
+        }));
+      });
   }
 
-  attach($root: HTMLElement) {
-    $root.innerHTML = '';
-    $root.appendChild(this.view.root);
+  private getOptions(word: string, options: Product[]) {
+    return options.filter((product: any) => {
+      const regex = new RegExp(word, 'gi');
+      return product.name.match(regex);
+    });
   }
 }
 
-export const searchComp = new SearchPage(html);
+export const searchComp = new SearchTips(html);
